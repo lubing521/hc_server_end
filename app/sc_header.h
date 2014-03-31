8d4f859c0760dfb376a30a224ee8d3908b65d461
ZHAO Yao <zhaoyao@ruijie.com.cn>
2014-03-31 10:27:03 +0800

    Change SC_NGX_ROOT_PATH_LEN to more smart type

--- app/sc_header.h
+++ app/sc_header.h
@@ -17,7 +17,7 @@
 #define __SC_HEADER_H__
 
 #define SC_NGX_ROOT_PATH                "/data/usb0/"
-#define SC_NGX_ROOT_PATH_LEN            11
+#define SC_NGX_ROOT_PATH_LEN            (sizeof(SC_NGX_ROOT_PATH) - 1)
 #define SC_NGX_DEFAULT_IP_ADDR          "127.0.0.1"
 #define SC_NGX_DEFAULT_PORT             8089
 
